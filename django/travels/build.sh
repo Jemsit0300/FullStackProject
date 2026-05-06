c7d2720dec6a5d63bb5cca8e79c8a2f35995158f
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-05-06 11:22:28 +0500

    Superuser otomatik oluşturma eklendi

--- django/travels/build.sh
+++ django/travels/build.sh
@@ -4,3 +4,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin1').exists():
+    User.objects.create_superuser('admin1', 'admin1@example.com', 'admin1')
+"
